a0dbfba1e75083c900ad2e97c98284f2f5050ce1
Unknown <shinchri7@gmail.com>
2018-03-17 18:37:32 -0500

    created form views and added some templates

--- Blog_MySite/blog/views.py
+++ Blog_MySite/blog/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
+from django.views.generic import (TemplateView, ListView, DetailView,
+                                  CreateView, UpdateView, DeleteView)
+from .forms import CommentForm, PostForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from blog.models import Post, Comment
+from django.utils import timezone
+from django.urls import reverse_lazy
+
 
 # Create your views here.
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    #  how to be listed
+    def get_queryset(self):
+        #  '-' in published_date changes to descending order
+        return Post.objects.filter(published_date__lte=timezone.now).order_by('-published_date')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+#  @ blah blah works for function view only, use mixin
+class CreatePostView(CreateView, LoginRequiredMixin):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('create_date')
